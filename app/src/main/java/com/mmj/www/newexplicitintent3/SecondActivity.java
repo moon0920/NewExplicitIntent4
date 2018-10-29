adb8bfa20b9485a027dd8767abf8394c40429ac3
Moon1 <moon0920@dreamwiz.com>
2018-10-29 15:52:12 +0900

    Initial commit

--- app/src/main/java/com/mmj/www/newexplicitintent3/SecondActivity.java
+++ app/src/main/java/com/mmj/www/newexplicitintent3/SecondActivity.java
@@ -1,13 +1,27 @@
 package com.mmj.www.newexplicitintent3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button btnSecond = findViewById(R.id.btnSecond);
+        btnSecond.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.btnSecond){
+            Intent intentsecond = new Intent(this,ThirdActivity.class);
+            startActivity(intentsecond);
+        }
+
     }
 }
